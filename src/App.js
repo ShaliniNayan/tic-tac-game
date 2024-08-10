33266c5b7657a2df4bd86ec416c9578153460fed
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-08-11 00:04:35 +0530

    Update aap js files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import './App.css';
+import React, { useState } from 'react';
+import Board from './components/Board';
+import '././index.css';
 
 function App() {
+  const [reset, setReset] = useState(false);
+  const [winner, setWinner] = useState('');
+
+  const resetBoard = () => {
+    setReset(true);
+  };
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        {/* <img src={logo} className='App-logo' alt='logo' /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`winner ${winner !== '' ? '' : 'shrink'}`}>
+        <div className='winner-text'>{winner}</div>
+        <button onClick={() => resetBoard()}>Reset Board</button>
+      </div>
+      <Board
+        reset={reset}
+        setReset={setReset}
+        winner={winner}
+        setWinner={setWinner}
+      />
     </div>
   );
 }
