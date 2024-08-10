6682c7cbf4d2fafa99380152f78b2aefddcaf482
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2024-08-10 19:05:50 +0530

    Add and update bord as it will render squares and manages their state

---
+++ src/components/Board.js
@@ -0,0 +1,67 @@
+// This component renders the squares and manages their state.
+
+import React, { useState } from 'react';
+import Square from './Square';
+
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const handleClick = (index) => {
+    const newSquares = squares.slice();
+    if (calculateWinner(newSquares) || newSquares[index]) {
+      return;
+    }
+    newSquares[index] = isXNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setIsXNext(!isXNext);
+  };
+
+  const renderSquare = (index) => {
+    return <Square value={squares[index]} onClick={() => handleClick(index)} />;
+  };
+
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${isXNext ? 'X' : 'O'}`;
+
+  return (
+    <div>
+      <div className='text-center mb-4 text-xl'>{status}</div>
+      <div className='grid grid-cols-3 gap-1'>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+export default Board;
